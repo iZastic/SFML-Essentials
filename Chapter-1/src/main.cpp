b2af1b50f4a983a3dcaadfc71569c45fdbeae320
iZastic <izastic@gmail.com>
2015-11-18 05:27:58 -0700

    Initial commit. SFML Window

---
+++ Chapter-1/src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by iZastic on 11/18/2015.
+//
+
+#include <SFML/Window.hpp>
+
+int main(int argc, char **argv)
+{
+    sf::Window window(sf::VideoMode(300, 200), "SFML Essentials");
+
+    return 0;
+}
