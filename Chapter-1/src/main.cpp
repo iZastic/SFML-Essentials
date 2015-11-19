bb0b740dd27d71f196261b78b84b0deca9e6f04f
iZastic <izastic@gmail.com>
2015-11-19 03:56:30 -0700

    A little game

--- Chapter-1/src/main.cpp
+++ Chapter-1/src/main.cpp
@@ -4,31 +4,64 @@
 
 #include <SFML/Graphics.hpp>
 
+void initShape(sf::RectangleShape &shape, sf::Vector2f const &pos, sf::Color const &color);
+
 int main(int argc, char **argv) {
-  sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Essentials");
+  sf::RenderWindow window(sf::VideoMode(480, 180), "Bad Squares");
   // Set target frames per second
   window.setFramerateLimit(60);
 
-  sf::RectangleShape rect(sf::Vector2f(50, 50));
+  sf::Vector2f pos(50, 90);
+  sf::RectangleShape player(sf::Vector2f(50, 50));
+  initShape(player, pos, sf::Color::Green);
+
+  sf::RectangleShape target(sf::Vector2f(50, 50));
+  initShape(target, sf::Vector2f(400, 90), sf::Color::Blue);
+
+  sf::RectangleShape enemy(sf::Vector2f(50, 100));
+  initShape(enemy, sf::Vector2f(250, 90), sf::Color::Red);
 
   // Game loop
   while (window.isOpen()) {
+    // Handle events
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
 
-    // Update
-    rect.rotate(1.5);
-    rect.move(sf::Vector2f(1, 0));
+    // Always moving right
+    player.move(1, 0);
+
+    // Player input
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+      player.move(0, 1);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+      player.move(0, -1);
+
+    // Target reached. You win. Exit game
+    if (player.getGlobalBounds().intersects(target.getGlobalBounds()))
+      window.close();
+
+    // You lose. Restart
+    if (player.getGlobalBounds().intersects(enemy.getGlobalBounds()) ||
+        player.getPosition().x >= window.getSize().x)
+      player.setPosition(pos);
 
     // Render
     window.clear(sf::Color::Black);
-    window.draw(rect);
+    window.draw(player);
+    window.draw(target);
+    window.draw(enemy);
     window.display();
 
   }
 
   return 0;
+}
+
+void initShape(sf::RectangleShape &shape, sf::Vector2f const &pos, sf::Color const &color) {
+  shape.setFillColor(color);
+  shape.setPosition(pos);
+  shape.setOrigin(shape.getSize() * 0.5f);
 }
